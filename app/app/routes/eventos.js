1e6f77707078ef02b7dd45f1cfdde28fdc9183ba
João Pedro Baptista <joaosbaptista@hotmail.com>
2019-03-10 01:59:32 -0300

    Coreção de links

--- app/app/routes/eventos.js
+++ app/app/routes/eventos.js
@@ -1,5 +1,5 @@
 module.exports = (application) => {
 	application.get('/eventos', async (req, res) => {
-		application.app.controllers.eventoController(application, req, res);
+		application.app.controllers.eventoController.index(application, req, res);
 	});
 }
